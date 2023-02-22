c92fb951437ed8e5eed50433c19e423d73a024c9
Zem1991 <felipelopeszem@hotmail.com>
2023-02-22 17:25:58 -0300

    Resolvido o problema do CORS - agora o frontend consegue chamar as APIs.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApiDbContext>(options => options.UseNpgsql(conn));
+builder.Services.AddCors();
+//builder.Services.AddCors(options =>
+//{
+//    options.AddDefaultPolicy(
+//        policy =>
+//        {
+//            policy.WithOrigins("http://localhost:4200/");
+//        }
+//    );
+//});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +31,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.UseCors();
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins separated with comma
+    .AllowCredentials()); // allow credentials
 
 //app.UseHttpsRedirection();
 app.UseAuthorization();
